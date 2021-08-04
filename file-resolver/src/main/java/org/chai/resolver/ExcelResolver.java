e4c1768448574cac171bb5c9ae4576294b861a1a
sdoxiaobaomei <18829355062@163.com>
2021-08-04 11:13:48 +0800

    Update ExcelResolver.java

--- file-resolver/src/main/java/org/chai/resolver/ExcelResolver.java
+++ file-resolver/src/main/java/org/chai/resolver/ExcelResolver.java
@@ -142,7 +142,7 @@ public class ExcelResolver {
         filterOutput("推广单元名称", "1组收藏加购","壹者", "418薇亚");
         filterOutput("推广单元名称", "嗖扬达人收藏加购", "SY达人");
         filterOutput("推广单元名称", "德收藏加购","德");
-        filterOutput("推广单元名称", "达人收藏加购", "达人", "618开屏");
+        filterOutput("推广单元名称", "达人收藏加购", "达人", "618开屏", "729壹者达人测试链接");
         filterOutput("推广单元名称", "冰晶绿_数智收藏加购", "冰晶绿_数智");
     }
 
