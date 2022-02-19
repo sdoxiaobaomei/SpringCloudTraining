182e5b48991e7a6b0b038a3df82b76629953351d
sdoxiaobaomei <18829355062@163.com>
2022-02-19 17:23:06 +0800

    ui upadte

--- vue-ui/file-resolver/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp, VueElement } from 'vue'
-import App from './App.vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-
-createApp(App).mount('#app')
-// createApp(Counter).mount('#')
